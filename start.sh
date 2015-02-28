32c5cd8fe93dbb9cd0eae672a363c1e37f3483e9
Dumitru Cantea <canteadumitru@gmail.com>
2015-02-28 15:23:49 +0200

    Bulgaria + control scripts

---
+++ start.sh
@@ -0,0 +1,10 @@
+
+### start weather web app
+
+(export COUNTRY=md PORT=4101 && forever start index.js)
+
+(export COUNTRY=ro PORT=4102 && forever start index.js)
+
+(export COUNTRY=ru PORT=4103 && forever start index.js)
+
+(export COUNTRY=bg PORT=4104 && forever start index.js)
