a0465d47c565802ad0df5738d174b65f463e86f5
Dumitru Cantea <canteadumitru@gmail.com>
2015-03-03 14:10:53 +0200

    start node with params

--- start.sh
+++ start.sh
@@ -3,14 +3,14 @@
 
 #forever stopall
 
-(export COUNTRY=md PORT=4101 && forever start index.js --max-old-space-size=64)
+(export COUNTRY=md PORT=4101 && forever start index.js -c "node --stack_size=100 --max_old_space_size=100")
 
-(export COUNTRY=ro PORT=4102 && forever start index.js --max-old-space-size=64)
+(export COUNTRY=ro PORT=4102 && forever start index.js -c "node --stack_size=100 --max_old_space_size=100")
 
-(export COUNTRY=ru PORT=4103 && forever start index.js --max-old-space-size=64)
+(export COUNTRY=ru PORT=4103 && forever start index.js -c "node --stack_size=100 --max_old_space_size=100")
 
-(export COUNTRY=bg PORT=4104 && forever start index.js --max-old-space-size=64)
+(export COUNTRY=bg PORT=4104 && forever start index.js -c "node --stack_size=100 --max_old_space_size=100")
 
-(export COUNTRY="in" PORT=4105 && forever start index.js --max-old-space-size=64)
+(export COUNTRY="in" PORT=4105 && forever start index.js -c "node --stack_size=100 --max_old_space_size=100")
 
-(export COUNTRY=it PORT=4106 && forever start index.js --max-old-space-size=64)
+(export COUNTRY=it PORT=4106 && forever start index.js -c "node --stack_size=100 --max_old_space_size=100")
