70a7207136f10ed3295159a33ddb935e00d8bfab
Dumitru Cantea <canteadumitru@gmail.com>
2015-03-03 14:00:44 +0200

    --max-old-space-size=64

--- start.sh
+++ start.sh
@@ -3,14 +3,14 @@
 
 #forever stopall
 
-(export COUNTRY=md PORT=4101 && forever start index.js)
+(export COUNTRY=md PORT=4101 && forever start index.js --max-old-space-size=64)
 
-(export COUNTRY=ro PORT=4102 && forever start index.js)
+(export COUNTRY=ro PORT=4102 && forever start index.js --max-old-space-size=64)
 
-(export COUNTRY=ru PORT=4103 && forever start index.js)
+(export COUNTRY=ru PORT=4103 && forever start index.js --max-old-space-size=64)
 
-(export COUNTRY=bg PORT=4104 && forever start index.js)
+(export COUNTRY=bg PORT=4104 && forever start index.js --max-old-space-size=64)
 
-(export COUNTRY="in" PORT=4105 && forever start index.js)
+(export COUNTRY="in" PORT=4105 && forever start index.js --max-old-space-size=64)
 
-(export COUNTRY=it PORT=4106 && forever start index.js)
+(export COUNTRY=it PORT=4106 && forever start index.js --max-old-space-size=64)
