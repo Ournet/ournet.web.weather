6d568eb8bc6b1373222425566b6ec244415afbc2
Dumitru Cantea <canteadumitru@gmail.com>
2015-03-04 11:07:48 +0200

    fix multi countries

--- start.sh
+++ start.sh
@@ -3,14 +3,16 @@
 
 #forever stopall
 
-(export COUNTRY=md PORT=4101 && forever start index.js -c "node --max-old-space-size=100")
+(export PORT=4101 && forever start index.js -c "node")
 
-(export COUNTRY=ro PORT=4102 && forever start index.js -c "node --max-old-space-size=100")
+# (export COUNTRY=md PORT=4101 && forever start index.js -c "node --max-old-space-size=100")
 
-(export COUNTRY=ru PORT=4103 && forever start index.js -c "node --max-old-space-size=100")
+# (export COUNTRY=ro PORT=4102 && forever start index.js -c "node --max-old-space-size=100")
 
-(export COUNTRY=bg PORT=4104 && forever start index.js -c "node --max-old-space-size=100")
+# (export COUNTRY=ru PORT=4103 && forever start index.js -c "node --max-old-space-size=100")
 
-(export COUNTRY="in" PORT=4105 && forever start index.js -c "node --max-old-space-size=100")
+# (export COUNTRY=bg PORT=4104 && forever start index.js -c "node --max-old-space-size=100")
 
-(export COUNTRY=it PORT=4106 && forever start index.js -c "node --max-old-space-size=100")
+# (export COUNTRY="in" PORT=4105 && forever start index.js -c "node --max-old-space-size=100")
+
+# (export COUNTRY=it PORT=4106 && forever start index.js -c "node --max-old-space-size=100")
