83a379ba113261751d170baa5fb3176ffa889638
Dumitru Cantea <canteadumitru@gmail.com>
2015-11-30 18:47:01 +0200

    env init

--- start.sh
+++ start.sh
@@ -1,18 +1,2 @@
 
-### start weather web app
-
-#forever stopall
-
-(export PORT=4101 && forever start index.js -c "node")
-
-# (export COUNTRY=md PORT=4101 && forever start index.js -c "node --max-old-space-size=100")
-
-# (export COUNTRY=ro PORT=4102 && forever start index.js -c "node --max-old-space-size=100")
-
-# (export COUNTRY=ru PORT=4103 && forever start index.js -c "node --max-old-space-size=100")
-
-# (export COUNTRY=bg PORT=4104 && forever start index.js -c "node --max-old-space-size=100")
-
-# (export COUNTRY="in" PORT=4105 && forever start index.js -c "node --max-old-space-size=100")
-
-# (export COUNTRY=it PORT=4106 && forever start index.js -c "node --max-old-space-size=100")
+pm2 stop ./pm2.json && pm2 start ./pm2.json
