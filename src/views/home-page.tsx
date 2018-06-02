912a4fe43eb9dc9b93f34fb66a60276bbe83ba62
Dumitru Cantea <canteadumitru@gmail.com>
2018-06-02 22:20:47 +0300

    deleted schemas

--- src/views/home-page.tsx
+++ src/views/home-page.tsx
@@ -6,25 +6,30 @@ import { LocalesNames } from '../locales';
 import * as util from 'util';
 import { PlaceHelper } from '../data/place-helper';
 // import Breadcrumb from './components/breadcrumb';
+import Header from './components/header';
 
 export default class HomePage extends React.Component<PageViewData> {
     render() {
-        const { page, __, locale } = this.props;
+        const props = this.props;
+        const { page, __, locale } = props;
         const countryName = __('country_' + locale.country);
         const inCountryName = PlaceHelper.inCountryName(countryName, locale.lang);
         page.title = util.format(__(LocalesNames.home_title_format), inCountryName);
         page.description = util.format(__(LocalesNames.weather_in_cn_summary), inCountryName);
         // const headerElements = page.headerElements = page.headerElements || [];
         return (
-            <PageLayout {...this.props}>
-                <div className='o-layout'>
-                    <div className='o-layout__item'>
+            <PageLayout {...props}>
+                <Header {...props} />
+                <main>
+                    <div className='o-layout'>
+                        <div className='o-layout__item'>
 
-                    </div>
-                    <div className='o-layout__item'>
+                        </div>
+                        <div className='o-layout__item'>
 
+                        </div>
                     </div>
-                </div>
+                </main>
             </PageLayout>
         )
     }
